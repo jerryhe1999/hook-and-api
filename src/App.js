9a847d94bbbcd0fdd9abe3d4833479f64c4ac6a8
JerryHe <mijerry1999@outlook.com>
2022-02-07 08:18:43 -0800

    class second update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-02-07 01:09:41
- * @LastEditTime: 2022-02-07 01:52:28
+ * @LastEditTime: 2022-02-07 07:18:52
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \cs499\hook-and-api\src\App.js
@@ -9,11 +9,12 @@
 import logo from './logo.svg';
 import './App.css';
 import MyComponentCL from './MyComponentCL';
+import MyComponentHO from './MyComponentHO';
 
 function App() {
   return (
     <div className="App">
-      <MyComponentCL />
+      <MyComponentHO />
     </div>
   );
 }
--- src/MyComponentHO.js
+++ src/MyComponentHO.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-02-07 01:54:00
- * @LastEditTime: 2022-02-07 02:11:11
+ * @LastEditTime: 2022-02-07 07:44:10
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \cs499\hook-and-api\src\MyComponentHO.js
@@ -9,18 +9,13 @@
 
 import React, { useEffect, useState } from 'react';
 
-query = 'cpp'
-
 function MyComponentHO(){
-    const [error, setError ] = useState(null);
-    const [isLoaded, setIsLoaded ] = useState(false);
-    const [items, setItems ] = useState([]);
 
     const [ repos, setRepos ] = useState([]);
 
     async function fetchSearchResults() {
         const response = await fetch(
-            '`https://api.github.com/search/repositories?q=${query}`'
+            'https://api.github.com/search/repositories?q=${query}'
         );
         const responseData = await response.json();
         setRepos(responseData.items || []);
@@ -28,11 +23,18 @@ function MyComponentHO(){
     }
 
     useEffect( () => {
-        if (query) {
             fetchSearchResults();
-        }
-    }, [ query ])
+    })
 
+    return (
+        <ul>
+            {repos.map(reop => (
+                <li key={reop.id}>
+                    <a href={repos.html_url}>{repos.full_name}</a>
+                </li>
+            ))}
+        </ul>
+    )
 }
 
 export default MyComponentHO;
