e7affb0850f0b95e431e33d59c7bc149344d0419
JerryHe <mijerry1999@outlook.com>
2022-02-07 08:23:16 -0800

    class

--- src/MyComponentHO.js
+++ src/MyComponentHO.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-02-07 01:54:00
- * @LastEditTime: 2022-02-07 07:44:10
+ * @LastEditTime: 2022-02-07 08:22:50
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \cs499\hook-and-api\src\MyComponentHO.js
@@ -9,21 +9,31 @@
 
 import React, { useEffect, useState } from 'react';
 
-function MyComponentHO(){
+function MyComponentHO(query){
 
     const [ repos, setRepos ] = useState([]);
-
-    async function fetchSearchResults() {
-        const response = await fetch(
-            'https://api.github.com/search/repositories?q=${query}'
-        );
-        const responseData = await response.json();
-        setRepos(responseData.items || []);
-        
-    }
+    const [ isLoading, setIsLoading ] = useState(false);
+    const [ isError, setIsError ] = useState(false);
 
     useEffect( () => {
-            fetchSearchResults();
+        async function fetchSearchResults() {
+            let responseBody = {};
+            setIsLoading(true);
+            setIsError(false);
+            try {
+                const response = await fetch(
+                    `https://api.github.com/search/repositories?q=${query}`
+                );
+                responseBody = await response.json();
+            }
+            catch(e){
+                
+            }
+            setRepos(responseData.items || []);
+            
+        }
+        
+        fetchSearchResults();
     })
 
     return (
